8e5acab009189a5789e5ab9036da88f7f492116c
BrevinGeelan <brevin_geelan@DESKTOP-PNBKD3G>
2023-03-20 20:10:10 -0700

    initial push

---
+++ 7.js
@@ -0,0 +1,20 @@
+// Secure the Vault Steps
+
+// 1. Add img to html file 
+// 2. Make img background img using css
+// 3. Write first string for the initial message.
+// 4. Use alert in js for first string to make the message alert to the user.
+// 5. Make variable using / operator to get first digit
+// 6. Make variable using * operator to get second digit
+// 7. Make variable using - operator to get third digit
+// 8. Use alert to display vault code to user.
+
+const a1 = "you have received this message because you have been chosen to open an important vault. Here is the secret combination.."
+alert(a1) 
+let num1 = 300 / 30
+let num2 = 4 * 10
+let num3 = 55 - 16
+console.log(num1)
+console.log(num2)
+console.log(num3)
+alert(num1 + '-' + num2 + '-' + num3)
